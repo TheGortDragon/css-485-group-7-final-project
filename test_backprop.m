e1ca86f7769ac02ae9a5761f034fdb340af90d73
ysubbagh <ydns10@gmail.com>
2024-02-28 18:59:28 -0800

    all 1s :(

---
+++ test_backprop.m
@@ -0,0 +1,54 @@
+import BackPropLayer.*
+
+%% setup data
+%training data
+trainData = readmatrix('data/train.csv', 'Range', 'C2:ADF27456');
+trainData = trainData / 255;
+trainLabel = readmatrix('data/train.csv', 'Range', 'B2:B27456');
+%testing data
+testData = readmatrix('data/test.csv', 'Range', 'B2:ADE7173');
+testData = testData / 255;
+%get id labels
+testID = readmatrix('data/test.csv', 'Range', 'A2:A7173');
+
+
+%% setup network
+network = BackPropLayer(size(trainData, 2), 20, 1, 0.01);
+network.outputLayer.transferFunc = "logsig";
+network.hiddenLayer.transferFunc = "logsig";
+
+
+%% do the training
+epoch = 5;
+for rounds = 1:epoch
+    for i = 1:size(trainData, 1)
+        % Get the ith input pattern and target patterns
+        inputPattern = trainData(i, :);
+        targetPattern = trainLabel(i);
+
+        % Train the network with the current input and target pattern
+        network = network.train(targetPattern', inputPattern', 1);
+    end
+end
+
+%disp(network.hiddenLayer.weights);
+%disp(network.outputLayer.weights);
+
+%% classify test data
+testClass = zeros(size(testID));
+for i = 1:size(testData, 1)
+    input = testData(i, :);
+    output = network.compute(input');
+    testClass(i) = output;
+end
+
+
+%% output
+% Define the column names and the file name
+columnNames = {'id', 'label'};
+fileName = 'backpropClassification.csv';
+% Create a table with the output data and column names
+outputTable = array2table([testID, testClass], 'VariableNames', columnNames);
+% Write the table to a CSV file
+writetable(outputTable, fileName);
+

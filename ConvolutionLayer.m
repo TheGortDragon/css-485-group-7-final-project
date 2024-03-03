7ed6bd3b8df2c01c9b06190b42e30011e17cbbaf
ysubbagh <ydns10@gmail.com>
2024-03-02 16:07:04 -0800

    update forward for depth, num of filters

--- ConvolutionLayer.m
+++ ConvolutionLayer.m
@@ -1,4 +1,4 @@
-classdef ConvolutionLayer < Layer % for a depth 0 input network
+classdef ConvolutionLayer < Layer % for a (depth) 1 input network
 
     properties
         input % has sub variables !
@@ -7,13 +7,15 @@ classdef ConvolutionLayer < Layer % for a depth 0 input network
             % size, results
         kernelW % weights for the kernel
         bias
+        depth % number of filters
     end
 
     methods
         %constructor
-        function this = ConvolutionLayer(inputSize, kernelSize) %kernel size is the same as output size
+        function this = ConvolutionLayer(inputSize, kernelSize, depth) %kernel size is the same as output size
+            this.depth = depth;
             this.input.size = inputSize;
-            this.kernelW = rand(kernelSize);
+            this.kernelW = rand(kernelSize, kernelSize, depth);
             this.output.size = inputHeight - kernelSize + 1;
             this.bias = rand(this.output.size);
         end
@@ -21,9 +23,12 @@ classdef ConvolutionLayer < Layer % for a depth 0 input network
         % forward function (does not include any activation)
         function output = forward(this, input)
             this.input.lastResult = input; % to use in backward
-            
-            output = conv2(input, this.kernelW, 'valid');
-            output = output + this.bias;
+            this.output.result = zeros(this.output.size);
+            for i = 1:this.depth
+                convResult = conv2(input, this.kernelW(:, :, i), 'valid');
+                this.output.result(i, :, :) = convResult + this.bias(i);
+            end
+            output = this.output.result;
         end
 
         % backward function

e66977fc0a534d3967be5aa270217b3efb312d31
ysubbagh <ydns10@gmail.com>
2024-03-08 18:18:49 -0800

    add validation for report

--- conv_testing.m
+++ conv_testing.m
@@ -7,10 +7,6 @@ testData = readmatrix('data/test.csv', 'Range', 'B2:ADE7173');
 %get id labels (to use for output file)
 testID = readmatrix('data/test.csv', 'Range', 'A2:A7173');
 
-%% normalize data?
-% scale to 0-1?
-% zero out values below a threshold (~0.3) ?
-
 %% reformat data (784x1 -> 28x28)
 % training data
 numImages = size(trainData, 1);
@@ -50,6 +46,9 @@ for i = 1:numImages
     test3D(:, :, i) = reshape(testData(i, :), imgSize);
 end
 
+%% normalize data?
+% scale to 0-1
+% thresholding
 train3D = train3D / 255;
 train3D(train3D < 0.3) = 0;
 train3D(train3D >= 0.7) = 1;
@@ -62,13 +61,39 @@ train3D(train3D >= 0.7) = 1;
 cnn = CNN();
 
 %% train network
-cnn.train(train3D(:,:,:), label2D(:,:), 3, 12, outputSize, 5, .01);
+cnn.train(train3D(:,:,:), label2D(:,:), 3, 12, outputSize, 20, .01);
 
-%% validate network performance
+%% validate network performance and abillity w dirty
+% setup noise and accuracy
+noiseLevels = [0 50 100 200 400];
+accuracyMatrix = zeros(length(noiseLevels),1);
 
+% test accuracy at each of the noise levels at same amount at training
+for i = 1:length(noiseLevels)
+    noiseLevel = noiseLevels(i);
+    correctCount = 0;
+    % iter through the images in test data
+    for k = 1:size(train3D, 3)
+        %get patterns
+        input = train3D(:, :, k);
+        target = label2D(:, k);
+
+        % add noise to input data
+        noisyInput = addNoise(input, noiseLevel);
+
+        %classify
+        output = cnn.predict(noisyInput);
+
+        %check correctness
+        if isCorrect(output, target)
+            correctCount = correctCount + 1;
+        end
+    end
+
+    accuracyMatrix(i) = (correctCount / size(train3D, 3)) * 100;
+end
 
 %% test network
-
 testResults = zeros(size(testID)); %classification (w forward) of test data
 
 for i = 1:size(testID, 1)
@@ -81,13 +106,48 @@ for i = 1:size(testID, 1)
     testResults(i, 1) = num;
 end
 
-
 %% produce figs for doc ?
-
-
+%accuracy chart
+figure;
+hold on;
+plot(noiseLevels, accuracyMatrix, '-o', 'LineWidth',2);
+hold off;
+xticks(noiseLevels);
+xticklabels({'0', '50', '100', '200', '400'});  % Set custom labels for the x-ticks
+grid on;
+xlabel('Number Of Pixels Flipped');
+ylabel('Classification Accuracy (%)');
+title('Network Performance of A Convolutional Neural Network With Noisy Inputs');
 
 %% create table and export
 columnNames = {'id', 'label'};
 fileName = 'convResults.csv';
 outputTable = array2table([testID, testResults], 'VariableNames', columnNames);
 writetable(outputTable, fileName);
+
+%% helper functions
+
+% addNoise to a vector, distort it 
+function pvec = addNoise(pvec, num)
+    % ADDNOISE Add noise to "binary" vector
+    % pvec pattern vector (-1 and 1)
+    % num number of elements to flip randomly
+    % Handle special case where there's no noise
+    if num == 0
+        return;
+    end
+    % first, generate a random permutation of all indices into pvec
+    inds = randperm(length(pvec));
+    % then, use the first n elements to flip pixels
+    pvec(inds(1:num)) = -pvec(inds(1:num));
+end 
+
+%check if the output matches the target
+function correct = isCorrect(target, actual)
+    % Check if all elements in the vectors are equal, correct one hot code
+    if all(target == actual)
+        correct = true; % Vectors are equal
+    else
+        correct = false; % Vectors are not equal
+    end
+end
